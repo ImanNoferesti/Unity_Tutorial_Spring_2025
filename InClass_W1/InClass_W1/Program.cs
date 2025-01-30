f1ed5ca4871cabeed4eb61abc3a5b2398eafcb49
kadetanke <tanke@uiowa.edu>
2025-01-30 13:37:12 -0600

    updates
    01/30/2025 13:37

--- InClass_W1/InClass_W1/Program.cs
+++ InClass_W1/InClass_W1/Program.cs
@@ -1,9 +1,15 @@
 ﻿// 1. Syntax and Declaring Variables
 /*
-  * Write a program to declare variables for your name, age, and a decimal value for your height in meters or inches.
-  * Print them out in a single sentence.
-*/
+ * Write a program to declare variables for your name, age, and a decimal value for your height in meters or inches.
+ * Print them out in a single sentence.
+ */
+string name = "Kade";
+int age = 20;
+double height = 1.88; //meters
 
+Console.WriteLine($"My name is {name}, my age is {age}, my height is {height} meters.");
+
+  
 
 
 
@@ -11,13 +17,22 @@
 // 2. Display Variables and Declare Multiple Variables
 // Declare three integers (x, y, z) on a single line, assign values to them, and display their sum.
 
-
+int x = 2, y = 1, z = 5;
+int sum = x + y + z;
+Console.WriteLine($"The sum of {x}, {y}, and {z} is {sum}");
 
 
 
 
 // 3. Min and Max Values
 // Write a program that displays the minimum and maximum values for int and double data types.
+string maxInt = int.MaxValue;
+string minInt = int.MinValue;
+string maxDouble = double.MaxValue;
+string minDouble = double.MinValue;
+
+Console.WriteLine($"Maximum for Integer: {maxInt}, Minimum for Integer: {minInt}, For double: {maxDouble}, {minDouble}");
+
 
 
 
@@ -26,12 +41,20 @@
 
 // 4. Strings
 /*
-*  Write a program that asks the user for their first name and last name.
-*  Print the length of their first name.
-*  Concatenate the user's first name and last name and display the full name.
-*  Print the user's initials using the full name
-*/
-
+ *  Write a program that asks the user for their first name and last name.
+ *  Print the length of their first name.
+ *  Concatenate the user's first name and last name and display the full name.
+ *  Print the user's initials using the full name
+ */
+Console.WriteLine("Enter your first name:");
+string firstName = Console.ReadLine();
+Console.WriteLine("Enter your last name:");
+string lastName = Console.ReadLine();
+char firstInitial = firstName[0];
+char lastInitial = lastName[0];
+string Name = (firstName + " " + lastName);
+Console.WriteLine(Name);
+Console.WriteLine(firstInitial + " " + lastInitial);
 
 
 
@@ -43,8 +66,21 @@
 * Create a simple calculator that performs addition, subtraction, multiplication, and division between two numbers.
 * Use assignment operators.
 */
+Console.WriteLine("Enter a number:");
+string firstNumber = Console.ReadLine();
+double firstDouble = System.Convert.ToDouble(firstNumber);
+Console.WriteLine("Enter another number:");
+string secondNumber = Console.ReadLine();
+double secondDouble = System.Convert.ToDouble(secondNumber);
+double sumNumber = firstDouble + secondDouble;
+Console.WriteLine("The sum of the numbers is " + sumNumber);
+double subNumber = firstDouble - secondDouble;
+Console.WriteLine("The subtraction of the numbers is " + subNumber);
+double multiplyNumber = firstDouble * secondDouble;
+Console.WriteLine("The multiplication of the numbers is " + multiplyNumber);
+double divideNumber = firstDouble / secondDouble;
+Console.WriteLine("The division of the numbers is " + divideNumber);
 
- 
  
  
 
@@ -55,7 +91,18 @@
 * Check if the user is old enough to drive (at least 18 years old) and has a license.
 * Use logical AND and OR operators to handle different cases.
 */
- 
+Console.WriteLine("Enter your age:");
+string age = Console.ReadLine();
+int ageInt = System.Convert.ToInt(age);
+Console.WriteLine("Do you have a driver's license? True or False:");
+string driversLicense = Console.ReadLine();
+bool licenseBool = System.Convert.ToBool(driversLicense);
+if ageInt >= 18 and driversLicense == true
+    Console.WriteLine("You are able to drive")
+    else Console.WriteLine("You are not able to drive");
+    
+
+
  
  
  
