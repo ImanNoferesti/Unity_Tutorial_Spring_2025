11838f782b215ba47a208304de84a39c3b14c3e5
cmooreIA <42701305+cmooreIA@users.noreply.github.com>
2025-02-15 14:27:43 -0600

    I don't know what these changes are

--- Week2/InClassPractice/Practice/obj/Debug/net9.0/Practice.AssemblyInfo.cs
+++ Week2/InClassPractice/Practice/obj/Debug/net9.0/Practice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a71a9d995052bf20414f5221dd33b94c4f0ddbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd0869d82addc9b2a01b45d909bdf150c8be2377")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
