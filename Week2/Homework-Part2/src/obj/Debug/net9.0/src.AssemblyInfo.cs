61c542dbf2c5ca0ae71a88b48adb3dc4b6460c8d
kadetanke <tanke@uiowa.edu>
2025-02-19 11:37:15 -0600

    start 2/19

--- Week2/Homework-Part2/src/obj/Debug/net9.0/src.AssemblyInfo.cs
+++ Week2/Homework-Part2/src/obj/Debug/net9.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fa6a1b33aa6152d790c853dcbe92f51de0c4e5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd398aa1538995f24775dd29469adccc97489f95")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
