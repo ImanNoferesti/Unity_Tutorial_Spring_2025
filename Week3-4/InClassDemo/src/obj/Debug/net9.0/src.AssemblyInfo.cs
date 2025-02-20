5a5b49a5ebe451f80d5bcc344d7ef4d7748b395f
ariii-k <80087197+ariii-k@users.noreply.github.com>
2025-02-20 15:38:10 -0600

    update

--- Week3-4/InClassDemo/src/obj/Debug/net9.0/src.AssemblyInfo.cs
+++ Week3-4/InClassDemo/src/obj/Debug/net9.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+764cdddf61d9389416ec394ab4e69fd84b304192")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16a8ebbf2a34e444b04f915ac287949805894ce0")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
