9d1c25336b7be8ce100a61a0480c2e814515e635
ariii-k <80087197+ariii-k@users.noreply.github.com>
2025-02-27 16:16:56 -0600

    updates
    updates

--- Week3-4/InClassDemo/src/LibraryManagement/Book.cs
+++ Week3-4/InClassDemo/src/LibraryManagement/Book.cs
@@ -1,8 +1,34 @@
 namespace src.LibraryManagement;
 
-public class Book
-{
-    // ********* Step 1 *********
-    public string Title;
-    private string _author;
-}
+// public class Book
+// {
+//     // ********* Step 1 *********
+//     public string Title;
+//     private string _author;
+// }
+//
+
+// // ***Step 3 ***
+//     
+// private string _title;
+// private string _author;
+//
+// public string Title {
+//     get{ return = _title;}
+//     set{ _title = value;}
+// }
+//
+// public string Author 
+// {get {
+//     return _author; }}
+//     
+// public Book(string title, string author);
+// {
+//     this._title = title;
+//     this._author = author;
+// }
+//
+// // ***Step 4 ***
+// public void DisplayInfo($"Book: {_title} by {_author}");
+
+//**Step 5***
--- Week3-4/InClassDemo/src/Program.cs
+++ Week3-4/InClassDemo/src/Program.cs
@@ -1,12 +1,48 @@
 ﻿using src.LibraryManagement;
 
-// ********* Step 1 *********
-Book myBook = new Book();
+// // ********* Step 1 *********
+// Book myBook = new Book();
+//
+// Console.WriteLine($"Book: {myBook.Title} by {myBook.Author}");
+//
+// myBook.Title = "Atomic Habits";
+// myBook.Author = "James Clear";
+//
+// Console.WriteLine($"Book: {myBook.Title} by {myBook.Author}");
+//
+//
+// myBook.Title = "New Title";
+//
+//
+// public string Title {
+//     get;
+//     set;
+// }
+// public string Author {
+//     get {return _author;}
+// }
+//
+// public Book (string Title, string Author);
+// {
+//     this.Title = title;
+//     this.Author = author;
+// }
+    //
+    //
+    // Book myBook = new Book (author: "Atomic Habits", title: "James Clear");
+    // Console.WriteLine($"Book: {myBook.Title} by {myBook.Author}");
+    //
+    // // ***Step 4 ***
+    // Book myBook = new Book (author: "Atomic Habits", title: "James Clear");
+    // myBook.DisplayInfo();
+    //
+    //**Step 5***
 
-Console.WriteLine($"Book: {myBook.Title} by {myBook.Author}");
-
-myBook.Title = "Atomic Habits";
-myBook.Author = "James Clear";
-
-Console.WriteLine($"Book: {myBook.Title} by {myBook.Author}");
-
+    public string Title
+    {
+        get{return _title;}
+    
+    set {_title = value;}
+    }
+    
+    public string 
