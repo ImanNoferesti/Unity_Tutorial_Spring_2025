7805cb54aa971aaed00b58b2c1a46328724477ca
Iman <109436129+ImanNoferesti@users.noreply.github.com>
2025-02-27 10:52:57 -0600

    Added Week4 Materials

--- Week3-4/InClassDemo/src/obj/Debug/net9.0/src.AssemblyInfo.cs
+++ Week3-4/InClassDemo/src/obj/Debug/net9.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+764cdddf61d9389416ec394ab4e69fd84b304192")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+368f457236e9a3e52519643b43eee0b210f365c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
