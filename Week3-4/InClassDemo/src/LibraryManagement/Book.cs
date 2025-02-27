ad1eb07e58f73dd534b912a0ecc0e551cee477e3
Fernando J Castillo <fernando-j-castillo@uiowa.edu>
2025-02-27 16:05:01 -0600

    update

--- Week3-4/InClassDemo/src/LibraryManagement/Book.cs
+++ Week3-4/InClassDemo/src/LibraryManagement/Book.cs
@@ -2,7 +2,47 @@ namespace src.LibraryManagement;
 
 public class Book
 {
-    public string Title;
-    public string Author;
+    // Step 1
+    //public string Title;
+    //public string Author;
 
+    // Step 2
+
+    // public string Title { get; set; }
+    //
+    // public string Author { get; }
+
+    //
+    // public Book(string title, string author)
+    // {
+    //     this.Title = title;
+    //     this.Author = author;
+    // }
+
+
+    // Step 3
+    private string _title;
+    private string _author;
+
+    public string Title
+    {
+        get { return _title; }
+        set { _title = value; }
+    }
+
+    public string Author
+    {
+        get { return _author; }
+    }
+
+    // Constructor
+
+    public Book(string title, string author)
+    {
+        this._title = title;
+        this._author = author;
+    }
 }
+
+    
+    
--- Week3-4/InClassDemo/src/Program.cs
+++ Week3-4/InClassDemo/src/Program.cs
@@ -2,15 +2,21 @@
 
 // ************ Step 1 ***************
 
-Book myBook = new Book();
-Console.WriteLine($"Book: {myBook.Title} by {myBook.Author}");
-
-myBook.Title = "Atomic Habits";
-
-myBook.Author = "Pedro Gonzales";
-
-Console.WriteLine($"Book: {myBook.Title} by {myBook.Author}");
+// Book myBook = new Book();
+// Console.WriteLine($"Book: {myBook.Title} by {myBook.Author}");
+//
+// myBook.Title = "Atomic Habits";
+//
+// myBook.Author = "Pedro Gonzales";
+//
+// Console.WriteLine($"Book: {myBook.Title} by {myBook.Author}");
 
 
 // Step 2: Improving Data Handling
 
+// * Step 3: Controlling Access
+// *      Raw data fields can sometimes lead to unintended modifications.
+// *      It would be wise to restrict direct access and instead provide a structured way to interact with book details.
+// *      Adjust the class so that its data is better protected, yet still accessible when needed.
+
+Book myBook = new Book("Atomic Habits", "James Clear")
