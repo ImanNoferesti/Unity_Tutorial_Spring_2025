5cbae1a154d8434abc70ab13821c152467314d6b
ariii-k <80087197+ariii-k@users.noreply.github.com>
2025-03-13 17:47:00 -0500

    changes

---
+++ Week5/In-Class-Solution/Assets/Scripts/Hierarchy.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Hierarchy : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log($"Root: {transform.root.name}");
+        Debug.Log($"Deepest Parent: {transform.parent.name}");
+        Debug.Log($"First Cube: {transform.root.Find("Cube (2)")}");
+        Debug.Log($"Deepest Child: {transform.root.Find("Cube (3)/Cube (5)/Cube (6)")}");
+        Debug.Log($"Child Count: {transform.root.childCount}");
+        Debug.Log($"Third Child: {transform.root.GetChild(2)}");
+        Debug.Log($"Root Index: {transform.root.GetSiblingIndex()}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
