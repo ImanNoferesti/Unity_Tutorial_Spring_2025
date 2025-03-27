022db1b11f5b4c3f35297bbca7a5498c9b34ee3b
Fernando <fernando-j-castillo@uiowa.edu>
2025-03-27 15:35:30 -0500

    update

--- Week2/Homework/src/obj/Debug/net9.0/src.AssemblyInfo.cs
+++ Week2/Homework/src/obj/Debug/net9.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd5f70b96b4fa0f28a79234247fbdd07c3c19017")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee1b383eb8ce1ae34a9e64c326693cc56add8b4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
