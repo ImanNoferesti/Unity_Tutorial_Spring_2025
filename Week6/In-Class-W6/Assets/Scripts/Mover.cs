971e2df977ea79703bb7d81be9143926bec5559d
Moore <cmmoor@uiowa.edu>
2025-04-03 17:31:18 -0500

    04.03.25 5:31 pm

--- Week6/In-Class-W6/Assets/Scripts/Mover.cs
+++ Week6/In-Class-W6/Assets/Scripts/Mover.cs
@@ -1,35 +1,85 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.TestTools.Utils;
 
 public class Mover : MonoBehaviour
 {
     public float MovementSpeed;
     public float RotationSpeed;
+    public float MaxAngle;
+    private float _currentYRotation;
 
-  
+    void Start()
+    {
+        _currentYRotation = transform.localEulerAngles.y;
+    }
 
-    // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
 
-        if (gameObject.tag == "FrontWheel")
+        // Rotation around x-axis
+        transform.Rotate(new Vector3(verticalInput, 0f, 0f), , MovementSpeed * Time.deltaTime);
+        
+        // Rotation around y-axis
+
+        if(Mathf.Abs(_currentYRotation + verticalInput * RotationSpeed) <= MaxAngle)
         {
-            transform.Rotate(new Vector3(verticalInput,0f,0f), MovementSpeed * Time.deltaTime);
-            transform.Rotate(new Vector3(0f,horizontalInput, 0f), RotationSpeed * Time.deltaTime);
+            transform.Rotate(new Vector3(0f, horizontalInput, 0f), RotationSpeed * Time.deltaTime);
+            _currentYRotation = horizontalInput * RotationSpeed * Time.deltaTime;
         }
-     
-        else
-        {
-            transform.Translate(new Vector3(0f, 0f, verticalInput) * MovementSpeed * Time.deltaTime);
-            transform.Rotate(new Vector3(0f,horizontalInput, 0f), RotationSpeed * Time.deltaTime);
-
-
-            // Debug.Log($"vertical: {verticalInput}");
-        }
-       
-
-
     }
+
+
+
+
+
+    //////////////// initial logic - not great /////////////////
+
+    // Update is called once per frame
+    // void Update()
+    // {
+    //     float horizontalInput = Input.GetAxis("Horizontal");
+    //     float verticalInput = Input.GetAxis("Vertical");
+
+    //     if (gameObject.tag == "Frontwheel")
+    //     {
+    //         transform.Rotate(new Vector3(verticalInput, 0f, 0f), MovementSpeed * Time.deltaTime);
+
+    //         // If rotating inside the boundary
+    //         if(transform.parent.transform.localEulerAngles.y <= 30f || transform.localEulerAngles.y >= 330f)
+    //         {
+    //             transform.parent.transform.Rotate(new Vector3(0f, horizontalInput, 0f), RotationSpeed * Time.deltaTime);
+    //         }
+
+    //         // Outside the boundary
+    //         else
+    //         {
+
+    //             if(transform.parent.transform.localEulerAngles.y > 30f && transform.localEulerAngles.y <= 180f)
+    //             {
+    //                 transform.parent.transform.localEulerAngles = new Vector3(0f, 29.9f, 0f);
+    //             }
+
+    //             if(transform.parent.transform.localEulerAngles.y > 180f && transform.localEulerAngles.y < 330f)
+    //             {
+    //                 transform.parent.transform.localEulerAngles = new Vector3(0f, 330f, 0f);
+    //             }
+    //         }
+
+    //     }
+
+    // else
+    // {
+    //     transform.Translate(new Vector3(0f, 0f, verticalInput) * MovementSpeed * Time.deltaTime);
+    //     transform.Rotate(new Vector3(0f,horizontalInput, 0f), RotationSpeed * Time.deltaTime);
+
+
+    //     // Debug.Log($"vertical: {verticalInput}");
+    // }
+
+
+
 }
+
