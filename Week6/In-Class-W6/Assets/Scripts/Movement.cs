f91d608d2545d2c23aaeca9d5b9888ddc701ac95
Fernando <fernando-j-castillo@uiowa.edu>
2025-04-03 17:29:08 -0500

    update

--- Week6/In-Class-W6/Assets/Scripts/Movement.cs
+++ Week6/In-Class-W6/Assets/Scripts/Movement.cs
@@ -4,48 +4,53 @@ public class Movement : MonoBehaviour
 {
     public float speed;
     public float rotationSpeed;
-    
-    
-    // Update is called once per frame
-    void Update()
-    {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        
-        float verticalInput = Input.GetAxis("Vertical");
-        
-        if(gameObject.tag == "frontTire")
-        {
-            transform.Rotate(new Vector3(verticalInput, 0f, 0f), speed * Time.deltaTime);
-            if(transform.localEulerAngles.y <= 30f || transform.localEulerAngles.y >= 330f)
-            {
-                transform.Rotate(new Vector3(0f, horizontalInput, 0f), rotationSpeed * Time.deltaTime);
-            }
 
-            else
-            {
-                if(transform.localEulerAngles.y > 30f && transform.localEulerAngles.y <= 180f)
-                {
-                    transform.localEulerAngles = new Vector3(0f, 30f, 0f);
-                }
+    public float MaxAngle;
+    private float _current
+    
+    //////////////////////////////////////////// INITIAL LOGIC //////////////////////////////////
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     float horizontalInput = Input.GetAxis("Horizontal");
+    //     float verticalInput = Input.GetAxis("Vertical");
+
+        
+    //     if(gameObject.tag == "frontTire")
+    //     {
+    //         transform.Rotate(new Vector3(verticalInput, 0f, 0f), speed * Time.deltaTime); // for the motion
+
+    //         // if rotating inside the boundary
+    //         if(transform.parent.transform.localEulerAngles.y <= 30f || transform.localEulerAngles.y >= 330f)
+    //         {
+    //             transform.parent.transform.Rotate(new Vector3(0f, horizontalInput, 0f), rotationSpeed * Time.deltaTime);
+    //         }
+
+    //         else
+    //         {
+    //             if(transform.parent.transform.localEulerAngles.y > 30f && transform.localEulerAngles.y <= 180f)
+    //             {
+    //                 transform.parent.transform.localEulerAngles = new Vector3(0f, 29.9f, 0f);
+    //             }
                 
-                if(transform.localEulerAngles.y > 30f && transform.localEulerAngles.y < 330f)
-                {
-                    transform.localEulerAngles = new Vector3(0f, 330f, 0f);
-                }
-            }
+    //             if(transform.parent.transform.localEulerAngles.y > 180f && transform.localEulerAngles.y < 330f)
+    //             {
+    //                 transform.parent.transform.localEulerAngles = new Vector3(0f, 330f, 0f);
+    //             }
+    //         }
 
 
-        }
+    //     }
 
-        else
-       {
+    //      else
+    //     {
 
-        transform.Translate(new Vector3 (0f, 0f, verticalInput) * speed * Time.deltaTime);
+    //      transform.Translate(new Vector3 (0f, 0f, verticalInput) * speed * Time.deltaTime);
 
-        transform.Rotate(new Vector3(0f, horizontalInput, 0f), rotationSpeed * Time.deltaTime);
-       } 
+    //      transform.Rotate(new Vector3(0f, horizontalInput, 0f), rotationSpeed * Time.deltaTime);
+    //     } 
         
 
 
-    }
+    // }
 }
