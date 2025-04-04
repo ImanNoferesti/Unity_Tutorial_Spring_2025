53002d89d02869ef4574e92f6fc6bfcf11f1fd82
Moore <cmmoor@uiowa.edu>
2025-04-04 12:02:57 -0500

    04.04.25 12:02 pm

--- Week6/In-Class-W6/Assets/Scripts/Mover.cs
+++ Week6/In-Class-W6/Assets/Scripts/Mover.cs
@@ -1,54 +1,74 @@
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.TestTools.Utils;
 
 public class Mover : MonoBehaviour
-{
+{   
+
     public float MovementSpeed;
-    public float RotationSpeed;
+    public float RotationAngle;
+
     public float MaxAngle;
     private float _currentYRotation;
 
     void Start()
     {
-        _currentYRotation = transform.localEulerAngles.y;
+        if(gameObject.tag == "Frontwheel")
+        {
+            _currentYRotation = transform.parent.transform.localEulerAngles.y;
+        }
     }
 
     void Update()
     {
+
+
         float verticalInput = Input.GetAxis("Vertical");
         float horizontalInput = Input.GetAxis("Horizontal");
 
-        // Rotation around x-axis
-        transform.Rotate(new Vector3(verticalInput, 0f, 0f), , MovementSpeed * Time.deltaTime);
-        
-        // Rotation around y-axis
-
-        if(Mathf.Abs(_currentYRotation + verticalInput * RotationSpeed) <= MaxAngle)
+        if(gameObject.tag == "Frontwheel")
         {
-            transform.Rotate(new Vector3(0f, horizontalInput, 0f), RotationSpeed * Time.deltaTime);
-            _currentYRotation = horizontalInput * RotationSpeed * Time.deltaTime;
+            // Rotation around x-axis
+            transform.Rotate(new Vector3(verticalInput, 0f, 0f), MovementSpeed * Time.deltaTime);
+
+            // Rotation around y-axis
+            if(Mathf.Abs(_currentYRotation + horizontalInput * RotationAngle * Time.deltaTime) <= MaxAngle)
+            {
+                Debug.Log($"horizontalInput: {horizontalInput}, currentY: {_currentYRotation}");
+                transform.parent.transform.Rotate(new Vector3(0f, horizontalInput, 0f), RotationAngle * Time.deltaTime);
+                _currentYRotation += horizontalInput * RotationAngle * Time.deltaTime;
+            }
         }
-    }
+
+         if(gameObject.tag == "Backwheel")
+         {
+            transform.Rotate(new Vector3(verticalInput, 0f, 0f), MovementSpeed * Time.deltaTime);
+         }
+   
+
+        if(gameObject.tag == "Main")
+        {
+            transform.Translate(new Vector3(0f, 0f, verticalInput) * MovementSpeed * Time.deltaTime);
+            Debug.Log($"Vertical: {verticalInput}");
+            transform.Rotate(new Vector3(0f, horizontalInput, 0f), RotationAngle * Time.deltaTime);
+        }
+
+     }
+
+    ///////////////////////////////////////// Initial Logic ////////////////////////////////////////////////
 
 
-
-
-
-    //////////////// initial logic - not great /////////////////
 
     // Update is called once per frame
-    // void Update()
-    // {
+    // void Update()    {
     //     float horizontalInput = Input.GetAxis("Horizontal");
     //     float verticalInput = Input.GetAxis("Vertical");
 
-    //     if (gameObject.tag == "Frontwheel")
+
+    //     if(gameObject.tag == "Frontwheel")
     //     {
     //         transform.Rotate(new Vector3(verticalInput, 0f, 0f), MovementSpeed * Time.deltaTime);
 
     //         // If rotating inside the boundary
-    //         if(transform.parent.transform.localEulerAngles.y <= 30f || transform.localEulerAngles.y >= 330f)
+    //         if(transform.parent.transform.localEulerAngles.y <= 30f || transform.parent.transform.localEulerAngles.y >= 330f)
     //         {
     //             transform.parent.transform.Rotate(new Vector3(0f, horizontalInput, 0f), RotationSpeed * Time.deltaTime);
     //         }
@@ -57,27 +77,22 @@ public class Mover : MonoBehaviour
     //         else
     //         {
 
-    //             if(transform.parent.transform.localEulerAngles.y > 30f && transform.localEulerAngles.y <= 180f)
+    //             if(transform.parent.transform.localEulerAngles.y > 30f && transform.parent.transform.localEulerAngles.y <= 180f)
     //             {
     //                 transform.parent.transform.localEulerAngles = new Vector3(0f, 29.9f, 0f);
     //             }
 
-    //             if(transform.parent.transform.localEulerAngles.y > 180f && transform.localEulerAngles.y < 330f)
+    //             if(transform.parent.transform.localEulerAngles.y > 180f && transform.parent.transform.localEulerAngles.y < 330f)
     //             {
     //                 transform.parent.transform.localEulerAngles = new Vector3(0f, 330f, 0f);
     //             }
     //         }
 
+
+
     //     }
 
-    // else
-    // {
-    //     transform.Translate(new Vector3(0f, 0f, verticalInput) * MovementSpeed * Time.deltaTime);
-    //     transform.Rotate(new Vector3(0f,horizontalInput, 0f), RotationSpeed * Time.deltaTime);
-
-
-    //     // Debug.Log($"vertical: {verticalInput}");
-    // }
+   
 
 
 
