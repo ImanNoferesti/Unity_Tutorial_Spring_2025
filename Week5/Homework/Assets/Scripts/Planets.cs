b547111a60ca5be2decbb565af2ce9126e56e133
kadetanke <tanke@uiowa.edu>
2025-04-05 09:03:56 -0500

    Week 5 HW Completed
    04/04/2025; 9:03AM

--- Week5/Homework/Assets/Scripts/Planets.cs
+++ Week5/Homework/Assets/Scripts/Planets.cs
@@ -14,17 +14,19 @@ public class Planets : MonoBehaviour
 
     public int count;
 
+    Vector3 Scale;
     
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Scale = new Vector3(1.0f, 1.0f, 1.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        count++;
         if (gameObject.tag == "CenterPlanet")
         {
             transform.Rotate(Vector3.right, RotateAngleCenter * Time.deltaTime);
@@ -35,35 +37,37 @@ public class Planets : MonoBehaviour
         }
         if (gameObject.tag == "Oddball")
         {
-            if (count < 125)
+            transform.parent.transform.Rotate(Vector3.forward, RotateAngleOddball * Time.deltaTime);
+            if (count % 300 > 150)
             {
-                transform.RotateAround(new Vector3(4, 0 ,0), Vector3.forward, RotateAngleOddball * Time.deltaTime);
-                count++;
-                Debug.Log(count);
+                transform.Translate(new Vector3(1, 1, 0) * MovementSpeed * Time.deltaTime);
             }
-            else if (count >= 125 && count < 250)
-            {
-                transform.RotateAround(new Vector3(0, 4, 0), Vector3.forward, RotateAngleOddball * Time.deltaTime);
-                count++;
-                Debug.Log(count);
-            }
-            else if (count >= 325 && count <525)
-            {
-                transform.RotateAround(new Vector3(-4, 0, 0), Vector3.forward, RotateAngleOddball * Time.deltaTime);
-                count++;
-                Debug.Log(count);
-            }
-            else if (count >= 525 && count <725)
-            {
-                transform.RotateAround(new Vector3(0, -4, 0), Vector3.forward, RotateAngleOddball * Time.deltaTime);
-                count++;
-                Debug.Log(count);
-            }
-            else if (count == 725)
-            {
-                count = 0;
-            }
+            else transform.Translate(new Vector3(-1, -1, 0) * MovementSpeed * Time.deltaTime);
             
         }
+        if (gameObject.tag == "TopBottomPlanets")
+        {
+            Debug.Log(transform.parent.transform.localEulerAngles);
+            if (transform.parent.transform.localEulerAngles.x <= 180f)
+            {
+                //transform.Translate(Vector3.forward * MovementSpeed * 10 * Time.deltaTime);
+                Scale.x = Scale.x - .01f;
+                Scale.y = Scale.y - .01f;
+                Scale.z = Scale.z - .01f;
+                Debug.Log($"Scale X: {Scale.x} Scale Y: {Scale.y} Scale Z: {Scale.z}");
+                transform.localScale = Scale;
+            }
+            else    
+            {
+                Scale.x = Scale.x + .01f;
+                Scale.y = Scale.y + .01f;
+                Scale.z = Scale.z + .01f;
+                transform.localScale = Scale;
+            }
+            
+            //transform.Translate(Vector3.back * MovementSpeed * 10 * Time.deltaTime);
+        }
+            
+        
     }
 }
