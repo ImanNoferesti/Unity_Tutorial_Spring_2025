febef3da9bfefb1817b08b7234be613b11074b27
Iman Noferesti <noferesti.iman@gmail.com>
2025-05-01 17:27:47 -0500

    Added Second Level Logics

---
+++ Mini-Project-Solution/Assets/Scripts/GameController.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public List<GameObject> DeActivatedObjs = new List<GameObject>();
+    public List<GameObject> DeActivatedObjs2 = new List<GameObject>();
+
+}
--- Mini-Project-Solution/Assets/Scripts/TriggerDetector.cs
+++ Mini-Project-Solution/Assets/Scripts/TriggerDetector.cs
@@ -3,12 +3,35 @@ using UnityEngine;
 public class TriggerDetector : MonoBehaviour
 {
 
+    GameController gameController;
+
+    void Start()
+    {
+        gameController = GameObject.Find("GameManager").GetComponent<GameController>();
+    }
+
     void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Player")
+        if(other.tag == "Player" && gameObject.tag != "Checkpoint" && gameController.tag == "Level1")
+        {
+            gameController.DeActivatedObjs.Add(gameObject);
+            gameObject.SetActive(false);
+        } 
+
+        else if(other.tag == "Player" && gameObject.tag != "Checkpoint" && gameController.tag == "Level2")
+        {
+            gameController.DeActivatedObjs2.Add(gameObject);
+            gameObject.SetActive(false);
+        }
+
+        else if(other.tag == "Player") 
         {
             gameObject.SetActive(false);
-        }        
+            other.gameObject.transform.position = new Vector3(-25f, 0.5f, -30f);
+            gameController.tag = "Level2";
+            other.GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
+            other.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        }       
     }
 }
 
