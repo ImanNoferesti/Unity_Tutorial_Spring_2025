4291c43c89d1c338abe7a23473ba246a337079f2
kadetanke <tanke@uiowa.edu>
2025-06-03 11:48:07 -0500

    Update

--- Mini-Project-Solution/Assets/Scripts/ButtonScript.cs
+++ Mini-Project-Solution/Assets/Scripts/ButtonScript.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ButtonScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Button myButton;
+    public TMP_Text myText;
+    private int _clickCount;
+
+
     void Start()
     {
+        myButton.onClick.AddListener(ButtonClick);
         
+
+    }
+    public void ButtonClick()
+    {
+        _clickCount++;
+        Debug.Log("Button is pressed");
+        myText.text = "Don't do that";
+        if (_clickCount == 2)
+        {
+            myText.text = "stoooop";
+        }
+        if (_clickCount > 3)
+        {
+            SceneManager.LoadScene(1);
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
